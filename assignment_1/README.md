203b227147328dbed94f72c29b9c9c46172a91ac
nanchin benle <nanchinbenle001@gmail.com>
2026-07-22 23:50:32 +1300

    completed Assignment 1

---
+++ assignment_1/README.md
@@ -0,0 +1,9 @@
+# ASSIGNMENT
+
+## description
+
+**_the Assignment is all about learning how to print output or creating a program that prints._**
+
+### objective
+
+to learn how to use the print method
---
+++ assignment_1/assign1.py
@@ -0,0 +1,11 @@
+"""this is a simple Python program that prints out a student's prints infromation."""
+
+print("WELCOME TO PYTHON")
+print("*" * 20)
+print("STUDENT PROFILE")
+print("*" * 20)
+print("NAME: Nanchin Benle")
+print("SCHOOL: PLASU")
+print("DEPARMENT: Computer Science")
+print("FAVOURITE FOOD: RICE")
+print("FAVOURITE PROGRAMMING LANGUAGE: Python")
